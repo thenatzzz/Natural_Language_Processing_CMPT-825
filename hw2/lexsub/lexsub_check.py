d76f4b895326352ea72b4273de2dc1fb25d426b6
njuthapr <nattapat_juthaprachakul@sfu.ca>
2020-10-03 21:19:33 -0700

    try gensum glove

--- hw2/lexsub/lexsub_check.py
+++ hw2/lexsub/lexsub_check.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
 
     with open(opts.ref, 'rt') as refh:
         ref_data = [str(x).strip() for x in refh.read().splitlines()]
-    # with open(opts.output, 'rt') as outh:
+
     with open(opts.output, 'rt',encoding='utf-8') as outh:
 
         out_data = [str(x).strip() for x in outh.read().splitlines()]
